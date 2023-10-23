eba53ec854d9ec9b69561dc2237380aa65302642
Boupouchi <81436882+Boupouchi@users.noreply.github.com>
2023-10-23 22:22:30 +0100

    Add files via upload

---
+++ ServeurRMI/src/service/MachineService.java
@@ -0,0 +1,153 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import dao.IDao;
+import entities.Machine;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import util.HibernateUtil;
+
+/**
+*
+* @author boupouchi
+*/
+public class MachineService extends UnicastRemoteObject implements IDao<Machine> {
+
+    public MachineService() throws RemoteException {
+        super();
+    }
+
+    @Override
+    public boolean create(Machine o) throws RemoteException {
+        Session session = null;
+        Transaction tx = null;
+        boolean etat = false;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            tx = session.beginTransaction();
+            session.save(o);
+            tx.commit();
+            etat = true;
+            session.close();
+        } catch (HibernateException ex) {
+            if (tx != null) {
+                tx.rollback();
+                etat = false;
+            }
+
+        } finally {
+            if (session != null) {
+                //session.close();
+            }
+        }
+        return etat;
+
+    }
+
+    @Override
+    public boolean update(Machine o) throws RemoteException {
+        Session session = null;
+        Transaction tx = null;
+        boolean etat = false;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            tx = session.beginTransaction();
+            session.update(o);
+            tx.commit();
+            etat = true;
+            session.close();
+        } catch (HibernateException ex) {
+            if (tx != null) {
+                tx.rollback();
+                etat = false;
+            }
+
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return etat;
+    }
+
+    @Override
+    public boolean delete(Machine o) throws RemoteException {
+        Session session = null;
+        Transaction tx = null;
+        boolean etat = false;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            tx = session.beginTransaction();
+            session.delete(o);
+            tx.commit();
+            etat = true;
+            //session.close();
+        } catch (HibernateException ex) {
+            if (tx != null) {
+                tx.rollback();
+                etat = false;
+            }
+
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return etat;
+    }
+
+    @Override
+    public Machine findById(int id) throws RemoteException {
+        Session session = null;
+        Transaction tx = null;
+        Machine machine = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            tx = session.beginTransaction();
+            machine = (Machine) session.get(Machine.class, id);
+            tx.commit();
+            
+        } catch (HibernateException ex) {
+            if (tx != null) {
+                tx.rollback();
+            }
+
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return machine;
+    }
+
+    @Override
+   public List<Machine> findAll() throws RemoteException {
+       Session session = null;
+        Transaction tx = null;
+        List<Machine> machines = new ArrayList<Machine>();
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            tx = session.beginTransaction();
+            machines = session.createQuery("from Machine").list();
+            tx.commit();
+        } catch (HibernateException ex) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            if (session != null) {
+                session.close();
+            }
+        }
+        return machines;
+    }
+
+}
---
+++ ServeurRMI/src/test/Test.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import dao.IDao;
+import entities.Machine;
+import java.rmi.RemoteException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.hibernate.Hibernate;
+import service.MachineService;
+import util.HibernateUtil;
+
+/**
+*
+* @author boupouchi
+*/
+public class Test {
+
+    public static void main(String[] args) {
+
+        
+      
+    }
+}
